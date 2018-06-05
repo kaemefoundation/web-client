19de0d57b09ffa9c92e2b7e691eefc1259a960e8
Madison Nelson <madison@Madisons-MacBook-Air-2.local>
2018-06-05 11:41:56 -0700

    adding puppeteer and jest

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import faker from 'faker';
+import puppeteer from 'puppeteer';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+test('h1 loads correctly', async () => {
+let browser = await puppeteer.launch({
+  headless: false,
+  slowmo: 80
 });
+let page = await browser.newPage();
+
+await page.goto('http://localhost:3000/');
+await page.waitForSelector('.red.button');
+
+const html = await page.$eval('.red.button', e => e.innerHTML);
+expect(html).toBe('Log in');
+
+const buttonClick = await page.click('.red.button');
+await page.waitForSelector('auth0-lock-submit');
+
+browser.close();
+}, 16000);
+
