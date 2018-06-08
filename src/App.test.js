67635b2083103a8646401e812cea7fa04b975e69
Sophia Shoemaker <szensius@gmail.com>
2018-06-08 09:12:38 -0700

    adding more tests -- for logging in and for orphanage form

--- src/App.test.js
+++ src/App.test.js
@@ -1,25 +1,80 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import faker from 'faker';
-import puppeteer from 'puppeteer';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import faker from "faker";
+import puppeteer from "puppeteer";
+import { auth0credentials } from "../testdata.js";
+let page;
+let browser;
 
-test('h1 loads correctly', async () => {
-let browser = await puppeteer.launch({
-  headless: false,
-  slowmo: 80
+const width = 1920;
+const height = 1080;
+const APP = "http://localhost:3000";
+beforeAll(async () => {
+	browser = await puppeteer.launch({
+		headless: false,
+		slowMo: 50,
+		args: [`--window-size=${width},${height}`]
+	});
+	page = await browser.newPage();
+	await page.setViewport({ width, height });
 });
-let page = await browser.newPage();
+afterAll(() => {
+	browser.close();
+});
 
-await page.goto('http://localhost:3000/');
-await page.waitForSelector('.red.button');
+function success(promise) {
+	return promise
+		.then(r => {
+			console.log("Succeed");
+			return true;
+		})
+		.catch(err => {
+			console.log("Error");
+			return false;
+		});
+}
 
-const html = await page.$eval('.red.button', e => e.innerHTML);
-expect(html).toBe('Log in');
+async function succeed(promise,description) {
+	console.log(description);
+	expect(await success(promise)).toBe(true);
+}
 
-const buttonClick = await page.click('.red.button');
-await page.waitForSelector('auth0-lock-submit');
+test(
+	"Log In to Kaeme",
+	async () => {
+		
 
-browser.close();
-}, 16000);
+		await page.goto("http://localhost:3000/");
+		await page.waitForSelector(".red.button");
 
+		const html = await page.$eval(".red.button", e => e.innerHTML);
+		expect(html).toBe("Log in");
+
+		const buttonClick = await page.click(".red.button");
+		await page.waitFor(5000);
+		await page.waitForSelector("input[name='email']");
+		await succeed(page.type("input[name='email']",auth0credentials.email),'Email address type');
+		const email = await page.$eval("input[name='email']",e=>e.value);
+		console.log(email);
+		await page.waitForSelector("input[name='password']");
+		
+		await succeed(page.type("input[name='password']",auth0credentials.password),'Password type');
+
+		await page.click(".auth0-lock-submit");
+		console.log("Returning back to Kaeme");
+		await page.waitForSelector("a.item.logo");
+		console.log("Back to Kaeme");
+
+	},
+	16000
+);
+test(
+	"Test Orphanage Form",
+	async () => {
+		
+		await page.goto("http://localhost:3000/orphanage");
+		console.log("Browse to orphanage");
+	},
+	16000
+);
---
+++ testdata-example.js
@@ -0,0 +1,1 @@
+export const auth0credentials = {email:"",password:""};
